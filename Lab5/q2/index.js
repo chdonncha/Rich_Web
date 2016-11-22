8b636d25cffd195cef477320be6a4da8b7dfe097
chdonncha <chdonncha@gmail.com>
2016-11-22 20:55:05 +0000

    tests

--- Lab5/q2/index.js
+++ Lab5/q2/index.js
@@ -1,6 +1,10 @@
 
 function canvas(){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
 
+    ctx.fillStype = "green";
+    ctx.fillRect(10,10,100,100);
 }
 
 (function() { canvas(); })();
