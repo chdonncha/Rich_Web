b58d9f183ad991ea2f07ce69d5dca2fb0a5ffbdf
chdonncha <chdonncha@gmail.com>
2016-11-22 02:40:24 +0000

    finished index.js

---
+++ Examples/stream2_counter/index.js
@@ -0,0 +1,20 @@
+import {Observable} from 'rsjx/Rx';
+
+const display = document.getElementById('display');
+const incButton = document.getElementById('increment');
+const decButton = document.getElementById('decrement');
+const rstButton = document.getElementById('reset');
+
+let counter = 1;
+
+const inc$ = Observable.fromEvent(incButton, 'click');
+inc$.subscribe(ev => { display.innerHTML = counter++; });
+
+const dec$ = Observable.fromEvent(decButton, 'click');
+dec$.subscribe(ev => { display.innerHTML = counter--; });
+
+const rst$ = Observable.fromEvent(rstButton, 'click');
+rst$.subscribe(ev => {
+  counter = 1;
+  display.innerHTML = 0;
+});
