4f9ad89f622fa111d4f287bb2d9a7a7e569809ba
chdonncha <chdonncha@gmail.com>
2016-11-22 21:55:22 +0000

    canvas example

--- Lab5/q2/index.js
+++ Lab5/q2/index.js
@@ -1,9 +1,11 @@
+import {Observable} from 'rxjs/Rx';
 
 function canvas(){
+    console.log("this is a test");
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
-    ctx.fillStype = "green";
+    ctx.fillStyle = "green";
     ctx.fillRect(10,10,100,100);
 }
 
